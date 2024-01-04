c024e1d645e2a524940b25339b3acb750c470156
Hacı Coşkun <hacicoskun07@gmail.com>
2024-01-04 20:35:33 +0300

    theme

---
+++ RestApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+                
+        }
+
+        
+    }
+}
---
+++ RestApi/MyTest.cs
@@ -0,0 +1,20 @@
+﻿namespace RestApi
+{
+
+    public abstract record MyTestBaseDto
+    {
+        protected internal int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        private int MyProperty4 { get; set; }  
+        public virtual bool MyMethod() { return true; }
+    }
+
+    public record MyTest2 : MyTestBaseDto
+    { 
+        public override bool MyMethod()
+        {
+            return base.MyMethod();
+        } 
+    }
+}
